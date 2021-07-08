b07fce01ecb03993b06a3d728df5953bd60978b4
beaniejoy <lhb1025@gmail.com>
2021-07-08 23:44:22 +0900

    update : security config 내용 추가
    - login, logout
    - remember-me
    - 해당 API Filter 과정

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Spring Security Demo
+
+```xml
+<dependency>
+    <groupId>org.springframework.boot</groupId>
+    <artifactId>spring-boot-starter-security</artifactId>
+</dependency>
+```
---
+++ src/main/java/io/security/basicsecurity/config/SecurityConfig.java
@@ -0,0 +1,96 @@
+package io.security.basicsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 인가 API
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                ;
+
+        // 인증 API
+        http
+                .formLogin()
+                // default : /login
+                // .loginPage("/loginPage")
+                .defaultSuccessUrl("/")
+                .failureUrl("/login")
+                .usernameParameter("userId")
+                .passwordParameter("pw")
+                .loginProcessingUrl("/login_proc")  // login form action url
+                // 로그인 성공 후 핸들러
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        System.out.println("authentication : " + authentication.getName());
+                        response.sendRedirect("/");
+                    }
+                })
+                // 로그인 실패 후 핸들러
+                .failureHandler(new AuthenticationFailureHandler() {
+                    @Override
+                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
+                        System.out.println("exception : " + e.getMessage());
+                        response.sendRedirect("/login");
+                    }
+                })
+                .permitAll()
+                ;
+
+        // 로그아웃 API
+        http
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .addLogoutHandler(new LogoutHandler() {
+                    @Override
+                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                        HttpSession session = request.getSession();
+                        session.invalidate();
+                    }
+                })
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        response.sendRedirect("/login");
+                    }
+                })
+                .deleteCookies("remember-me")
+        .and()
+                // remember me API
+                .rememberMe()
+                .rememberMeParameter("remember-me")
+                .tokenValiditySeconds(3600)
+                .alwaysRemember(false) // 기본적으로 false로 하는 것이 맞다.
+                .userDetailsService(userDetailsService)
+                ;
+    }
+}
