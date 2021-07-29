dbdbc83df1036d27fe22e3daa25fcdc126fe0a1b
beaniejoy <lhb1025@gmail.com>
2021-07-29 20:22:48 +0900

    update : csrf를 위한 config 별개 구성

--- src/main/java/io/security/basicsecurity/config/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/config/SecurityConfig.java
@@ -1,122 +1,21 @@
 package io.security.basicsecurity.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UserDetailsService userDetailsService;
-
-    public SecurityConfig(UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password("{noop}1111").roles("USER");
-        auth.inMemoryAuthentication().withUser("sys").password("{noop}1111").roles("SYS", "USER");
-        auth.inMemoryAuthentication().withUser("admin").password("{noop}1111").roles("ADMIN", "SYS", "USER");
-
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // 인가 API
         http
                 .authorizeRequests()
-                .antMatchers("/user").hasRole("USER")
-                .antMatchers("/admin/pay").hasRole("ADMIN")
-                .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
-                .anyRequest().authenticated()
-        ;
+                .anyRequest().permitAll();
 
-        // 인증 API
         http
-                .formLogin()
-                // default : /login
-                // .loginPage("/loginPage")
-                .defaultSuccessUrl("/")
-                .failureUrl("/login")
-                .usernameParameter("userId")
-                .passwordParameter("pw")
-                .loginProcessingUrl("/login_proc")  // login form action url
-                // 로그인 성공 후 핸들러
-                .successHandler(new AuthenticationSuccessHandler() {
-                    @Override
-                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                        System.out.println("authentication : " + authentication.getName());
-                        response.sendRedirect("/");
-                    }
-                })
-                // 로그인 실패 후 핸들러
-                .failureHandler(new AuthenticationFailureHandler() {
-                    @Override
-                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
-                        System.out.println("exception : " + e.getMessage());
-                        response.sendRedirect("/login");
-                    }
-                })
-                .permitAll()
-        ;
-
-        // 로그아웃 API
-        http
-                .logout()
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/login")
-                .addLogoutHandler(new LogoutHandler() {
-                    @Override
-                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-                        HttpSession session = request.getSession();
-                        session.invalidate();
-                    }
-                })
-                .logoutSuccessHandler(new LogoutSuccessHandler() {
-                    @Override
-                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                        response.sendRedirect("/login");
-                    }
-                })
-                .deleteCookies("remember-me")
-                .and()
-                // remember me API
-                .rememberMe()
-                .rememberMeParameter("remember-me")
-                .tokenValiditySeconds(3600)
-                .alwaysRemember(false) // 기본적으로 false로 하는 것이 맞다.
-                .userDetailsService(userDetailsService)
-        ;
-
-        // 동시 세션 제어 API
-        http
-                .sessionManagement()
-                .maximumSessions(1)                 // 최대 허용 가능 세션 수
-                .maxSessionsPreventsLogin(false)    // true: 최대 허용 세션 수 넘으면 새로 로그인 못함, false: 이전 사용자 세션을 만료시킴
-                .expiredUrl("/expired")             // 세션 만료시 이동할 페이지
-        ;
-
-        // 세션 고정 보호
-        http
-                .sessionManagement()                // default: changeSessionId
-                .sessionFixation().none();          // changeSessionId, none, migrateSession, newSession
-        ;
+                .formLogin();
     }
 }
